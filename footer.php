8ffefee7be07390dff1d8f084f330dc4b24447b9
saras <sarasobekova@azet.sk>
2026-03-17 11:04:09 +0100

    pridanie footer.php

---
+++ footer.php
@@ -0,0 +1,4 @@
+ <footer id="templatemo-footer">
+        <p>Copyright &copy; 2026 Your Company. Designed by <a href="https://templatemo.com" rel="nofollow" target="_blank">TemplateMo</a></p>
+    </footer>
+    <script src="templatemo-parallax-script.js"></script>
